a09435fc5a4c858a57115d74ff8ea5cefcf2a9a4
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-15 21:42:02 +0100

    simplify

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,6 @@ import { useState } from "react";
 export default function App() {
   const [snapshotData, setSnapshotData] = useState("");
 
-  // This function will be called whenever there is a change in the textarea.
-  const handleTextChange = (event) => {
-    const newText = event.target.value;
-    try {
-      const snapshot = JSON.parse(newText);
-      // Make sure to validate if the newText is a valid JSON and has the right structure for Tldraw
-      // Assuming here it is valid, we update the snapshotData state.
-      setSnapshotData(newText);
-    } catch (error) {
-      // If JSON.parse fails, it means the text is not valid JSON
-      // Depending on how you want to handle this, you may want to show an error to the user
-      console.error('Invalid JSON:', error);
-    }
-  };
-
-  // A function to load snapshot from the textarea
-  const loadSnapshotFromString = (editor, snapshotString) => {
-    try {
-      const snapshot = JSON.parse(snapshotString);
-      editor.store.loadSnapshot(snapshot);
-    } catch (error) {
-      console.error('Failed to load snapshot:', error);
-    }
-  };
-
   return (
     <div
       style={{
@@ -42,16 +17,10 @@ export default function App() {
       <div style={{ width: 500, height: 500 }}>
         <Tldraw>
           <SaveButton onSave={setSnapshotData} />
-          <LoadButton onLoad={(editor) => loadSnapshotFromString(editor, snapshotData)} />
         </Tldraw>
       </div>
       <div style={{ marginLeft: 20, flex: 1 }}>
-        <textarea
-          style={{ width: "100%", height: 500 }}
-          value={snapshotData}
-          onChange={handleTextChange}  // Add the onChange event listener
-          // Removing readOnly since we want to be able to edit the text
-        />
+        <textarea style={{ width: "100%", height: 500 }} value={snapshotData} />
       </div>
     </div>
   );
@@ -62,7 +31,13 @@ function SaveButton({ onSave }) {
 
   return (
     <button
-      style={{ position: "absolute", zIndex: 1000, right: 10, top: 10, backgroundColor: "lightblue" }}
+      style={{
+        position: "absolute",
+        zIndex: 1000,
+        right: 10,
+        top: 10,
+        backgroundColor: "lightblue",
+      }}
       onClick={() => {
         const snapshot = editor.store.getSnapshot();
         const stringified = JSON.stringify(snapshot);
@@ -73,21 +48,6 @@ function SaveButton({ onSave }) {
       }}
     >
       Save
-    </button>
-  );
-}
-
-function LoadButton({ onLoad }) {
-  const editor = useEditor();
-
-  return (
-    <button
-      style={{ position: "absolute", zIndex: 1000, right: 10, top: 50, backgroundColor: "lightgreen" }}
-      onClick={() => {
-        onLoad(editor);
-      }}
-    >
-      Load
     </button>
   );
 }
