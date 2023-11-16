befc1a2713a8f3b15e27653995bcc3ef51d2e427
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-16 15:53:55 +0100

    minimal

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
-import { Tldraw, useEditor } from "@tldraw/tldraw";
+import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
-import { useState } from "react";
+import { useCallback } from "react";
 
 export default function App() {
-  const [snapshotData, setSnapshotData] = useState("");
-
+  const handleMount = useCallback((app) => {}, []);
   return (
     <div
       style={{
-        display: "flex",
         position: "relative",
-        width: "100vw",
-        height: "100vh",
+        width: "1000px",
+        height: "1000px",
       }}
     >
-      <div style={{ width: 500, height: 500 }}>
-        <Tldraw>
-          <SaveButton onSave={setSnapshotData} />
-        </Tldraw>
-      </div>
-      <div style={{ marginLeft: 20, flex: 1 }}>
-        <textarea style={{ width: "100%", height: 500 }} value={snapshotData} />
-      </div>
+      <Tldraw onMount={handleMount} />
     </div>
-  );
-}
-
-function SaveButton({ onSave }) {
-  const editor = useEditor();
-
-  return (
-    <button
-      style={{
-        position: "absolute",
-        zIndex: 1000,
-        right: 10,
-        top: 10,
-        backgroundColor: "lightblue",
-      }}
-      onClick={() => {
-        const snapshot = editor.getSvg();
-        const stringified = JSON.stringify(snapshot);
-        localStorage.setItem("my-editor-snapshot", stringified);
-        console.log("Saved!");
-        console.log(stringified);
-        onSave(stringified);
-      }}
-    >
-      Save
-    </button>
   );
 }
