7252a206875724ae412322d3d79755608d282570
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-16 16:06:06 +0100

    simple square

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import { Tldraw } from "@tldraw/tldraw";
+import { Tldraw, createShapeId } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 import { useCallback } from "react";
 
 export default function App() {
-  const handleMount = useCallback((app) => {}, []);
+  const handleMount = (editor) => {
+    const id = createShapeId("hello");
+
+    editor.createShapes([
+      {
+        id,
+        type: "geo",
+        x: 128 + Math.random() * 500,
+        y: 128 + Math.random() * 500,
+        props: {
+          geo: "rectangle",
+          w: 100,
+          h: 100,
+          dash: "draw",
+          color: "blue",
+          size: "m",
+        },
+      },
+    ]);
+  };
+
   return (
     <div
       style={{
